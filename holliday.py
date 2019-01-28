4bb2c767db44ccb2aa75b0275ae4294666da0906
itzmesatheesh <46571920+itzmesatheesh@users.noreply.github.com>
2019-01-28 21:34:41 +0530

    Update and rename holliday.py to sum of squares.py

--- holliday.py
+++
@@ -1,5 +0,0 @@
-str1 = "Monday"
-if(str1 == "Saturday" or str1 == "Sunday"):
-    print("yes")
-else:
-    print("no")
---
+++ sum of squares.py
@@ -0,0 +1,6 @@
+a=input("")
+b=0
+for i in a:
+    c=int(i)
+    b=b+(c**2)
+print(b)

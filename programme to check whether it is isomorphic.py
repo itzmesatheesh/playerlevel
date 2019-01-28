c029fd91adeea4a1b41b99577e2ba173edd401a2
itzmesatheesh <46571920+itzmesatheesh@users.noreply.github.com>
2019-01-28 21:20:09 +0530

    Update programme to check whether it is isomorphic.py

--- programme to check whether it is isomorphic.py
+++ programme to check whether it is isomorphic.py
@@ -1,18 +1,18 @@
-dic = {}
-lis =[]
-d= "yes"
+a= {}
+b=[]
+d="yes"
 str1,str2 = map(str,input("").split())
 if(len(str1)!=len(str2)):
-    d ="no"
+    d="no"
 else:
     for i in range(len(str1)):
-        if str1[i] in lis:
-            temp = dic[str1[i]]
+        if str1[i] in b:
+            temp =a[str1[i]]
             if(temp == str2[i]):
                 d = "yes"
             else:
-                d = "no"
+                d="no"
         else:
-            lis.append(str1[i])
-            dic[str1[i]] = str2[i]
+            b.append(str1[i])
+            a[str1[i]]=str2[i]
 print(d)

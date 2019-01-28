92b7d118f27b81999329fed8bc9b38378ae93272
itzmesatheesh <46571920+itzmesatheesh@users.noreply.github.com>
2019-01-28 21:57:33 +0530

    Create prime factors sorted.py

---
+++ prime factors sorted.py
@@ -0,0 +1,19 @@
+import math
+a=int(input(""))
+def isprime(n):
+    if(n== 2):
+        return True
+    else:
+        for j in range(3,n):
+            if(n%j== 0):
+                return False
+        else:
+            return True
+c=0
+for i in range(2,a+1):
+    if(a%i== 0 and isprime(i)):
+        if(c==0):
+            c+=1
+            print(i,end="")
+        else:
+            print(" ",i,end="")

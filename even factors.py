4dddf8c84d2ff0501a328dbaeefb3b66268c325d
itzmesatheesh <46571920+itzmesatheesh@users.noreply.github.com>
2019-01-31 22:54:54 +0530

    Update even factors.py

--- even factors.py
+++ even factors.py
@@ -1,5 +1,5 @@
-a=int(input())
-for i in range(1,a+1):
-    if a%i==0:
-        if i%2==0:
-             print(i,end=" ")
+b=int(input())
+for j in range(1,j+1):
+    if b%j==0:
+        if j%2==0:
+             print(j,end=" ")
